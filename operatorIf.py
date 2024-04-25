a37c72f71892b45b077cdf4991ce523b5b6d74ba
rem12inboxru <166938917+rem12inboxru@users.noreply.github.com>
2024-04-25 22:53:21 +0800

    Add files via upload

---
+++ operatorIf.py
@@ -0,0 +1,22 @@
+x = 38
+print('Здрасьте')
+if x<0:
+    print('Вот')
+print('Пока')
+a, b = 10, 5
+if a>b:
+    print('a>b')
+if a>b and a>0:
+    print('успех')
+if (a>b) and (a>0 or b<1000):
+    print('успех')
+if 5<b and b<10:
+    print('успех')
+if '34' > '123':
+    print('успешный успех')
+if '123' > '12':
+    print('успешный успех')
+if [1, 2] > [1, 1]:
+    print('успех')
+if '6' != 7:
+    print('успех')
